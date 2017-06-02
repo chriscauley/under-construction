a30ebd4c24db73cbfa4a73811512080f5486e622
chriscauley <chris@lablackey.com>
2017-06-02 16:01:25 -0400

    using unrest.konsole and adding uC.active

--- under-construction.js
+++ under-construction.js
@@ -7,8 +7,8 @@
       remove: function(key) { return uR.storage.remove(PREFIX+key); },
       set: function(key,value) { return uR.storage.set(PREFIX+key,value); },
     },
-    hit: function() { console.log.apply(this,["HIT "].concat([].slice.apply(arguments))) },
-    miss: function() { console.log.apply(this,["MISS "].concat([].slice.apply(arguments))) },
+    hit: function() { konsole.log.apply(this,["HIT"].concat([].slice.apply(arguments))) },
+    miss: function() { konsole.log.apply(this,["MISS"].concat([].slice.apply(arguments))) },
     proxy: {
       send: window.XMLHttpRequest.prototype.send,
       open: window.XMLHttpRequest.prototype.open,
@@ -17,17 +17,17 @@
       read: true,
       write: true,
     },
+    active: true,
     getContext: function() { return { user_id: uR.auth.user && uR.auth.user.id } },
   };
   window.XMLHttpRequest.prototype.send = function() {
     // proxy around xhrReq.open(*arguments);
+    if (!uC.active) { return uC.proxy.send.apply(this, [].slice.call(arguments)); }
     if (uC.storage.has(this._uc_key)) {
-      uC.hit(this._uc_key);
+      uC.hit(this._uc_key,this._uc_url);
       var cached_response = uC.storage.get(this._uc_key);
-      console.log(cached_response);
       for (key in cached_response) {
         this[key] = cached_response[key];
-        console.log(this[key],cached_response[key]);
       };
       this.onload.apply(cached_response);
       return
@@ -38,22 +38,23 @@
   window.XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
     // proxy around xhrReq.open(method, url, async, user, password);
     var args = [].slice.call(arguments);
+    if (!uC.active) { return uC.proxy.open.apply(this, args); }
     this._uc_key = objectHash({
       method: method,
       url: url,
       context: uC.getContext(),
     });
-    console.log("key",this._uc_key);
+    this._uc_url = url;
     //uC.storage.remove(this._uc_key);
     return uC.proxy.open.apply(this, args);
   };
   uR.postAjax = function postAjax(request) {
+    if (!uC.active) { return }
     if (!request.status) { return }
     uC.storage.set(request._uc_key,{
       response: request.response,
       status: request.status,
     });
-    console.log("stored",uC.storage.get(request._uc_key));
   }
   
 })();
