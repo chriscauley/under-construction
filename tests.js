303a3f406a0fe1d6a5dd95361ef701a54fe4c9d0
chriscauley <chris@lablackey.com>
2017-06-06 20:58:42 -0400

    uc-test

--- tests.js
+++ tests.js
@@ -1,9 +1,9 @@
 (function() {
-  function _closeModal() { return uR.test.click("[ur-mask]") }
+  function _closeModal() { return uC.test.click("[ur-mask]") }
   function _useLogin() { return [
   ] };
   function login() {
-    var t = new uR.test.Test('Fail at login');
+    var t = new uC.test.Test('Fail at login');
     t.waitFor("auth-dropdown a")
       .click("auth-dropdown a")
       .waitFor("#id_username")
@@ -11,26 +11,26 @@
       .changeValue("#id_password","butler")
       .click("#submit_button")
   };
-  function waitThenClick() {
-    /*function wait(ms) {
-      var time;
-      return uR.test.watch(
-        function() { time = time || new Date(); return new Date()-time > ms },
-        { name: "waitOG "+ms+"ms" }
-      )
-    }*/
-    return Promise.resolve(function() { return true})//wait(1500,'start'))
-      .then(uR.test.click("auth-dropdown a"))
-      .then(uR.test.wait(100))
-      .then(uR.test.when(countTo(20),10,1000))
-      .then(uR.test.click("[ur-mask]"))
-      .then(uR.test.wait(300))
-      .then(uR.test.wait(400))
-      .then(uR.test.click("[title=Logs]"))
-      .then(uR.test.when(countTo(4000),10,1000))
-      .then(uR.test.wait(500))
+  function waitThenClick() { // broken
+    var t = new uC.test.Test('Wait Then click');
+
+      t.click("auth-dropdown a")
+      .wait(100)
+      .when(countTo(20),10,1000)
+      .click("[ur-mask]")
+      .wait(300)
+      .wait(400)
+      .click("[title=Logs]")
+      .when(countTo(4000),10,1000)
+      .wait(500)
+      .done(arstarst)
   }
 
+  // function doAll() {
+  //   t.test(login)
+  //     .test(openShoppinCart)
+  // }
+    
   function countTo(number) {
     return function countTo() { return !number--; }
   }
