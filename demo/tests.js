523c5f7a2dab1acbf2a96cbf8669513ee5f539e5
chriscauley <chris@lablackey.com>
2017-06-11 13:11:04 -0400

    updating demo to new syntax

--- demo/tests.js
+++ demo/tests.js
@@ -1,30 +1,38 @@
 (function() {
   function login(t) {
     t.do('Fail at login')
-      .waitFor("auth-dropdown a")
+      //.wait(100)
+      .wait("auth-dropdown a")
       .click("auth-dropdown a")
-      .waitFor("#id_username")
+      .wait("#id_username")
       .changeValue("#id_username","monkey")
       .changeValue("#id_password","butler")
       .click("#submit_button")
     t.done("bad username/password");
   };
-  function waitThenClick(t) { // broken
+  function waitThenClick(t) {
     t.do('Wait Then click')
       .click("auth-dropdown a")
       .wait(100)
-      .when(countTo(20),10,1000)
+      .wait(countTo(20),10,1000)
       .click("[ur-mask]")
       .wait(300)
       .wait(400)
       .click("[title=Logs]")
-      .when(countTo(4000),10,1000)
+      .wait(countTo(4000),10,1000)
       .wait(500)
       .done(arstarst)
   }
-    
+
+  function testURForm(t) {
+    var schema = ['first-name','last-name',{ name: 'email', type:'email'} ];
+    t.do('Making and testing ur-form element')
+      .then(function() { uR.mountElement('ur-form',{schema: schema}) })
+      .done()
+  }
+
   function countTo(number) {
     return function countTo() { return !number--; }
   }
-  konsole.addCommands(login,waitThenClick);
+  konsole.addCommands(login, waitThenClick, testURForm);
 })();
