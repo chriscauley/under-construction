6f4586470268877625881705c232a81d315f2fd6
chriscauley <chris@lablackey.com>
2017-06-23 19:16:07 -0400

    ajax method and test

--- demo/tests.js
+++ demo/tests.js
@@ -9,7 +9,11 @@
       .click("#submit_button")
     t.done("bad username/password");
   };
+
   function waitThenClick(t) {
+    function countTo(number) {
+      return function countTo() { return !number--; }
+    }
     t.do('Wait Then click')
       .click("auth-dropdown a")
       .wait(100)
@@ -27,9 +31,11 @@
   function formIsValid() {
     return !uC.find("#submit_button",'valid').classList.contains("disabled");
   }
+
   function formIsInvalid() {
     return uC.find("#submit_button",'invalid').classList.contains("disabled");
   }
+
   function testURForm(t) {
     var schema = ['first-name','last-name',{ name: 'email', type:'email'} ];
     t.do('Making and testing ur-form element')
@@ -44,8 +50,17 @@
       .done()
   }
 
-  function countTo(number) {
-    return function countTo() { return !number--; }
+  function testAjax(t) {
+    // Make an ajax request and change a state variable along the way
+    // make sure that state changes and ajax are in the right order.
+    var state = 0;
+    t.do("Testing ajax calls")
+      .assert(function() { return state == 0 })
+      .ajax('user.json',function(data,request) { state=1;return data.user.username == "chriscauley" })
+      .assert(function() { return state == 1 })
+      .then(function() { state=2 })
+      .assert(function() { return state == 2 })
+      .done("Ajax tested")
   }
-  konsole.addCommands(login, waitThenClick, testURForm);
+  konsole.addCommands(login, waitThenClick, testURForm, testAjax);
 })();
