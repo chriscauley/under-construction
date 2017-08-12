f5e039548dd40ca95d22fa57c25ca6ee1fb3bfc1
chriscauley <chris@lablackey.com>
2017-08-12 01:56:49 -0400

    updating tests to new promise-free test

--- demo/tests.js
+++ demo/tests.js
@@ -1,20 +1,20 @@
 (function() {
-  function login(t) {
-    t.do('Fail at login')
+  function login() {
+    this.do('Fail at login')
       .wait("auth-dropdown a")
       .click("auth-dropdown a")
       .wait("#id_username")
       .changeValue("#id_username","monkey")
       .changeValue("#id_password","butler")
       .click("#submit_button")
-    t.done("bad username/password");
+    this.done("bad username/password");
   };
 
-  function waitThenClick(t) {
+  function waitThenClick() {
     function countTo(number) {
       return function countTo() { return !number--; }
     }
-    t.do('Wait Then click')
+    this.do('Wait Then click')
       .click("auth-dropdown a")
       .wait(100)
       .wait(countTo(20),10,1000)
@@ -24,7 +24,6 @@
       .click("[title=Logs]")
       .wait(countTo(4000),10,1000)
       .wait(500)
-      .done(arstarst)
   }
 
   function formIsValid() {
@@ -35,9 +34,9 @@
     return uC.find("#submit_button",'invalid').classList.contains("disabled");
   }
 
-  function testURForm(t) {
+  function testURForm() {
     var schema = ['first-name','last-name',{ name: 'email', type:'email'} ];
-    t.do('Making and testing ur-form element')
+    this.do('Making and testing ur-form element')
       .then(function() { uR.mountElement('ur-form',{schema: schema}) })
       .wait("#submit_button.disabled")
       .changeValue("#id_first-name","monkey")
@@ -49,11 +48,11 @@
       .done()
   }
 
-  function testAjax(t) {
+  function testAjax() {
     // Make an ajax request and change a state variable along the way
     // make sure that state changes and ajax are in the right order.
     var state = 0;
-    t.do("Testing ajax calls")
+    this.do("Testing ajax calls")
       .assert(function() { return state == 0 })
       .ajax('user.json',function(data,request) { state=1;return data.user.username == "chriscauley" })
       .assert(function() { return state == 1 })
@@ -62,7 +61,7 @@
       .done("Ajax tested")
   }
 
-  function testCanvas(t) {
+  function testCanvas() {
     var canvas = document.createElement("canvas");
     document.getElementById("content").appendChild(canvas);
     canvas.width = 1;
@@ -83,7 +82,7 @@
       context.strokeStyle = '#003300';
       context.stroke();
     }
-    t.do("Fun with canvas")
+    this.do("Fun with canvas")
       .assertEqual(function() { return canvas.toDataURL() },onexone)
       .checkResults("onexone canvas always accept",function() { return canvas.toDataURL() })
       .then(drawCircle)
@@ -92,5 +91,5 @@
       .done("yay")
   }
 
-  konsole.addCommands(login, waitThenClick, testURForm, testAjax, testCanvas);
+  konsole.addCommands(login) //, waitThenClick, testURForm, testAjax, testCanvas);
 })();
